40075c515f19ac9377efece2742c8ccb91c90665
Chinmay Bhat <0310chinmay@gmail.com>
2023-04-20 16:19:54 +0530

    svm page completed

--- riota/src/pages/index.js
+++ riota/src/pages/index.js
@@ -2,11 +2,12 @@ import Contact from "./components/contact";
 import Home from "./components/home";
 import Terms from "./components/terms&cond";
 import Product from "./components/products"
+import Svm from "./components/svm"
 
 function index(){
   return(
     <div>
-    <Terms />
+    <Svm />
 
     </div>
   )
